a82d0d07eeffed72822dac0452ea0033d887caa8
Subhra Roy <subhra.roy@e-arc.com>
2021-12-13 14:11:44 +0530

    New Project Added

---
+++ CICDDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CICDDemo
+//
+//  Created by Subhra Roy on 13/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
