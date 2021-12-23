e2dd092e31375fa3cc21d2cd6448850f29bff06c
Subhra Roy <subhra.roy84@yahoo.in>
2021-12-23 21:01:02 +0530

    add derived data folder for build

---
+++ DerivedData/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-FriendListDemo.build/DerivedSources/Pods_FriendListDemo_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_FriendListDemoVersionString[];
+ extern const double Pods_FriendListDemoVersionNumber;
+
+ const unsigned char Pods_FriendListDemoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_FriendListDemo  PROJECT:Pods-1" "\n";
+ const double Pods_FriendListDemoVersionNumber __attribute__ ((used)) = (double)1.;
--- FriendListDemoUITests/FriendListDemoUITests.swift
+++ FriendListDemoUITests/FriendListDemoUITests.swift
@@ -24,19 +24,23 @@ class FriendListDemoUITests: XCTestCase {
 
     func testExample() throws {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
+        
+        XCTContext.runActivity(named: "screenshots") { (activity)  in
+            let app = XCUIApplication()
+            app.launch()
+            XCUIApplication().navigationBars["FriendListDemo.View"].buttons["Refresh"].tap()
+        }
+                                
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+        /*if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
-        }
+        }*/
     }
 }
